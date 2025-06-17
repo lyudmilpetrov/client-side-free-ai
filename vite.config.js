45ba3144a9c0a1bf60693f0e362728667a5a0744
lyudmilpetrov <lyudmilpetrov79@gmail.com>
2025-06-17 11:12:48 +0700

    s

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: '/client-side-free-ai/',
+    base: '/dist/',
     server: {
         port: 63716,
     }
