c2467c099ad3865cefc175844340f25991b575af
lyudmilpetrov <lyudmilpetrov79@gmail.com>
2025-06-24 23:06:17 +0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,39 @@ import { LlamaCpp } from "./llama-st/llama.js";
 
 const MODELS = [
   {
-    url: "https://huggingface.co/afrideva/TinyMistral-248M-SFT-v4-GGUF/resolve/main/tinymistral-248m-sft-v4.q8_0.gguf",
+    url:
+      "https://huggingface.co/afrideva/TinyMistral-248M-SFT-v4-GGUF/resolve/main/tinymistral-248m-sft-v4.q8_0.gguf",
     label: "tinymistral-248m-sft-v4 q8_0 (265.26 MB)",
   },
   {
-    url: "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
+    url:
+      "https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
     label: "TinyLlama/TinyLlama-1.1B-Chat-v1.0 Q4_K_M (669 MB)",
   },
   {
-    url: "https://huggingface.co/Qwen/Qwen1.5-1.8B-Chat-GGUF/resolve/main/qwen1_5-1_8b-chat-q3_k_m.gguf",
-    label: "Qwen/Qwen1.5-1.8B-Chat Q3_K_M (1.02 GB)",
-  },
-  {
-    url: "https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q4_1.gguf",
-    label: "stabilityai/stablelm-2-zephyr-1_6b Q4_1 (1.07 GB)",
-  },
-  {
-    url: "https://huggingface.co/TKDKid1000/phi-1_5-GGUF/resolve/main/phi-1_5-Q4_K_M.gguf",
+    url:
+      "https://huggingface.co/TKDKid1000/phi-1_5-GGUF/resolve/main/phi-1_5-Q4_K_M.gguf",
     label: "microsoft/phi-1_5 Q4_K_M (918 MB)",
   },
   {
-    url: "https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q3_K_M.gguf",
+    url:
+      "https://huggingface.co/Qwen/Qwen1.5-1.8B-Chat-GGUF/resolve/main/qwen1_5-1_8b-chat-q3_k_m.gguf",
+    label: "Qwen/Qwen1.5-1.8B-Chat Q3_K_M (1.02 GB)",
+  },
+  {
+    url:
+      "https://huggingface.co/stabilityai/stablelm-2-zephyr-1_6b/resolve/main/stablelm-2-zephyr-1_6b-Q4_1.gguf",
+    label: "stabilityai/stablelm-2-zephyr-1_6b Q4_1 (1.07 GB)",
+  },
+  {
+    url:
+      "https://huggingface.co/TheBloke/phi-2-GGUF/resolve/main/phi-2.Q3_K_M.gguf",
     label: "microsoft/phi-2 Q3_K_M (1.48 GB)",
+  },
+  {
+    url:
+      "https://huggingface.co/mradermacher/Qwen3-8B-Josiefied-iSMART-GGUF/resolve/main/Qwen3-8B-Josiefied-iSMART.Q2_K.gguf",
+    label: "Qwen3 8B Josiefied iSMART (3.28 GB)",
   },
 ];
 
@@ -36,8 +47,7 @@ export default function App() {
     console.error("⚠️ WebGPU not supported in this browser.");
     // You might show a fallback message/UI here.
   }
-  console.log("WebGPU supported:", navigator.gpu.wgslLanguageFeatures
-.size);
+  console.log("WebGPU supported:", navigator.gpu.wgslLanguageFeatures.size);
   const [modelUrl, setModelUrl] = useState(MODELS[0].url);
   const [prompt, setPrompt] = useState(DEFAULT_PROMPT);
   const [result, setResult] = useState("");
